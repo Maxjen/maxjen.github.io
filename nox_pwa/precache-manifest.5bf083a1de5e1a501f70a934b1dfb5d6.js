318f8a5be42ee75543f2ad9ccf9e8c962ee6487c
Maximilian Jensen <maxjen@gmail.com>
2018-05-10 18:43:03 +0200

    now uses workbox and typescript

---
+++ nox_pwa/precache-manifest.5bf083a1de5e1a501f70a934b1dfb5d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ea9192df2f9641be11ca651552798e5",
+    "url": "nox_pwa.webmanifest"
+  },
+  {
+    "revision": "ff2c16de3ed71da9655b0a955c7a3758",
+    "url": "index.html"
+  },
+  {
+    "revision": "865eeb56863464cb56bd5f28b619b396",
+    "url": "icons/icon-256.png"
+  },
+  {
+    "revision": "53c4c6878ed0b36bfb058a0bd66903a7",
+    "url": "icons/icon-128.png"
+  },
+  {
+    "revision": "8a5e4c56daf08067ed7a",
+    "url": "bundle.js"
+  }
+];
