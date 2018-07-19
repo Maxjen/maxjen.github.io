a61f73094e9bf10dffe4cc3356eee21a719d2fe2
Maximilian Jensen <max@localhost.localdomain>
2018-07-19 09:06:39 +0200

    editor update

---
+++ editor/precache-manifest.f663755e18ddd6dd1d916dd797f6d2f9.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = [
+  {
+    "revision": "448d0800c84b59380b9f1f3aac9e8c66",
+    "url": "zip-js/tests/test15.html"
+  },
+  {
+    "revision": "a8189b89d35d7cf62ce530270a5efe21",
+    "url": "zip-js/zlib-asm/codecs.js"
+  },
+  {
+    "revision": "9c7db9f5bcb57438bccbf048efda5eff",
+    "url": "zip-js/tests/test15.js"
+  },
+  {
+    "revision": "2ea9192df2f9641be11ca651552798e5",
+    "url": "nox_pwa.webmanifest"
+  },
+  {
+    "revision": "528462b8e1df7becafa25e0da5f5b672",
+    "url": "zip-js/zip.js"
+  },
+  {
+    "revision": "9365f982d209fc3691334f0a339def41",
+    "url": "zip-js/.jshintrc"
+  },
+  {
+    "revision": "d4e3a2a82db29526b20ce9314ce835fe",
+    "url": "zip-js/deflate.js"
+  },
+  {
+    "revision": "4abd21a23d6a5519b9b692f98d67e7c5",
+    "url": "zip-js/pako/codecs.js"
+  },
+  {
+    "revision": "fd952b261e960d0d7c8498569bee75d6",
+    "url": "zip-js/inflate.js"
+  },
+  {
+    "revision": "0b905934d7271bd0d03624ad556df7a0",
+    "url": "zip-js/mime-types.js"
+  },
+  {
+    "revision": "1844af1441424e7245f6231f47303b5f",
+    "url": "zip-js/tests/arraybuffer.js"
+  },
+  {
+    "revision": "562d23f345ba9d9325c5f8112c9248a3",
+    "url": "zip-js/tests/base64.js"
+  },
+  {
+    "revision": "eb70f64ee76b4132ac3c5df73fb99d76",
+    "url": "zip-js/tests/config.js"
+  },
+  {
+    "revision": "f6e3fea543f965f60dd3c6a4a522976e",
+    "url": "zip-js/tests/dataview.js"
+  },
+  {
+    "revision": "6b261544f39d23aa6a9fc8d70e6fada1",
+    "url": "zip-js/tests/lorem_store.zip"
+  },
+  {
+    "revision": "495347bbce33c4733bee7ef6b10b55b3",
+    "url": "zip-js/tests/lorem.txt"
+  },
+  {
+    "revision": "85b40beac94c4768a98ecfc53b028b27",
+    "url": "zip-js/tests/lorem.zip"
+  },
+  {
+    "revision": "c37900774979be9b9cd648e435936c8a",
+    "url": "zip-js/tests/lorem2.zip"
+  },
+  {
+    "revision": "760913ebacfe547d9a1ee99374d1e43f",
+    "url": "zip-js/tests/test1.html"
+  },
+  {
+    "revision": "9c3fd0ce1c274a4c38175090d072394f",
+    "url": "zip-js/tests/test10.html"
+  },
+  {
+    "revision": "3d5c6640d26862f466876ee9450273a3",
+    "url": "zip-js/tests/test1.js"
+  },
+  {
+    "revision": "95cede97388d3e764db282653ef62d37",
+    "url": "zip-js/tests/test10.js"
+  },
+  {
+    "revision": "695fa4fe8fc4224b284fcf8511a139e3",
+    "url": "zip-js/tests/test11.html"
+  },
+  {
+    "revision": "47f97c1c74e9213c6352bb14548383b6",
+    "url": "zip-js/tests/test11.js"
+  },
+  {
+    "revision": "e4382a322ea93c2bab6f1f118e648d74",
+    "url": "zip-js/tests/test12.js"
+  },
+  {
+    "revision": "14a4da082ac65356cbecf8df4414b8e4",
+    "url": "zip-js/tests/test12.html"
+  },
+  {
+    "revision": "bd12ea8442c67a4f97b9ce0b28e8e437",
+    "url": "zip-js/tests/test13.html"
+  },
+  {
+    "revision": "c70d91419a886e014b25138f70b13544",
+    "url": "zip-js/tests/test13.js"
+  },
+  {
+    "revision": "296f8b00d7cf6884c48db5afb60eaf96",
+    "url": "zip-js/tests/test14.html"
+  },
+  {
+    "revision": "46f6fac55a54ed8d327e985238aa13be",
+    "url": "zip-js/tests/test14.js"
+  },
+  {
+    "revision": "745bf6c481f36344f16d733984931523",
+    "url": "zip-js/zip-fs.js"
+  },
+  {
+    "revision": "13e57a9d99d4595fd1d1e4bbab39117b",
+    "url": "zip-js/zip-ext.js"
+  },
+  {
+    "revision": "2e73d6cd3680a08441c902efc890b157",
+    "url": "zip-js/tests/test16.html"
+  },
+  {
+    "revision": "6f737d33c3d1021d7dd2d6743e570715",
+    "url": "zip-js/tests/test16.js"
+  },
+  {
+    "revision": "dd09509d89e1d7aa889c43d1676081b8",
+    "url": "zip-js/tests/test17.html"
+  },
+  {
+    "revision": "35a40c3d64f37d9bd465ad5a17f740c9",
+    "url": "zip-js/tests/test17.js"
+  },
+  {
+    "revision": "f14846199ecd449d62b5f16d280753ac",
+    "url": "zip-js/tests/test18.html"
+  },
+  {
+    "revision": "10b73276ffb7b0cd65a21104183012b6",
+    "url": "zip-js/tests/test18.js"
+  },
+  {
+    "revision": "50494731bd84ea56be1e43f1bc8b8595",
+    "url": "zip-js/tests/test19-codecs-interop.html"
+  },
+  {
+    "revision": "797a0a6fe59a29e1e4d5ac2efdb1d926",
+    "url": "zip-js/tests/test2.html"
+  },
+  {
+    "revision": "82167edb99a23083b26d41e06b441a69",
+    "url": "zip-js/tests/test2.js"
+  },
+  {
+    "revision": "b42fd4058650004d06b8ac6891bdca81",
+    "url": "zip-js/tests/test3.html"
+  },
+  {
+    "revision": "7ded7a531448e1f662ce6cc35474bc62",
+    "url": "zip-js/tests/test3.js"
+  },
+  {
+    "revision": "f693d21ad54c88accd21adec719eb955",
+    "url": "zip-js/tests/test4.html"
+  },
+  {
+    "revision": "045647387799cc888d1493afd4c3588a",
+    "url": "zip-js/tests/test4.js"
+  },
+  {
+    "revision": "f8e3e28cf6cf19970abcc2955c68c0df",
+    "url": "zip-js/tests/test5.html"
+  },
+  {
+    "revision": "1e509807b93d850d0d5e07d9bff8d993",
+    "url": "zip-js/tests/test5.js"
+  },
+  {
+    "revision": "bae631cb51d20df528fa0c508aac4575",
+    "url": "zip-js/tests/test6.html"
+  },
+  {
+    "revision": "cddfe15e10a0c0f9c1e75b1feda65ba4",
+    "url": "zip-js/tests/test7.html"
+  },
+  {
+    "revision": "802870708b3875544e072a9c195dbcec",
+    "url": "zip-js/tests/test6.js"
+  },
+  {
+    "revision": "1aa6b66ea462b04ae4fd148cd083cde9",
+    "url": "zip-js/tests/test7.js"
+  },
+  {
+    "revision": "8dda4a5df1e7eeffd56eb129cd53f00c",
+    "url": "zip-js/tests/test8.html"
+  },
+  {
+    "revision": "39986a804afc492471b4df56269eb588",
+    "url": "zip-js/tests/test8.js"
+  },
+  {
+    "revision": "115fe9c337cf81ca80c57e82e3dbe8de",
+    "url": "zip-js/tests/test9.html"
+  },
+  {
+    "revision": "6f0ae386893d65177912f46bcd539d97",
+    "url": "zip-js/tests/test9.js"
+  },
+  {
+    "revision": "11f29c33e648fe58b3721bc523df05d5",
+    "url": "zip-js/tests/util.js"
+  },
+  {
+    "revision": "7d40d9ac5e628c8f4ab5e683b11e09ba",
+    "url": "zip-js/z-worker.js"
+  },
+  {
+    "revision": "0b4ed2782b7cfd5ea17a2907851e6010",
+    "url": "index.html"
+  },
+  {
+    "revision": "865eeb56863464cb56bd5f28b619b396",
+    "url": "icons/icon-256.png"
+  },
+  {
+    "revision": "53c4c6878ed0b36bfb058a0bd66903a7",
+    "url": "icons/icon-128.png"
+  },
+  {
+    "revision": "0d3908455ad640671b38",
+    "url": "bundle.js"
+  }
+];
