0deb79ebbb769cb2af3d5f30afe357e491425d51
Maximilian Jensen <maxjen@gmail.com>
2018-12-16 19:05:35 +0100

    reduced button padding on mobile

---
+++ character_preview/precache-manifest.b019ddb2c0901b9fec88933a7bde3b1a.js
@@ -0,0 +1,614 @@
+self.__precacheManifest = [
+  {
+    "revision": "45f3d282021100d4441e9d526574ed21",
+    "url": "images/m/m_f_l_leg_3.png"
+  },
+  {
+    "revision": "13e57a9d99d4595fd1d1e4bbab39117b",
+    "url": "zip-js/zip-ext.js"
+  },
+  {
+    "revision": "a8189b89d35d7cf62ce530270a5efe21",
+    "url": "zip-js/zlib-asm/codecs.js"
+  },
+  {
+    "revision": "528462b8e1df7becafa25e0da5f5b672",
+    "url": "zip-js/zip.js"
+  },
+  {
+    "revision": "745bf6c481f36344f16d733984931523",
+    "url": "zip-js/zip-fs.js"
+  },
+  {
+    "revision": "4f199d54eb29538b811b1c1c7c73489a",
+    "url": "images/m/m_f_r_arm_3.png"
+  },
+  {
+    "revision": "8bd74c1e7ac81ea17be0641ade1d89f9",
+    "url": "images/f/f_b_r_arm_3.png"
+  },
+  {
+    "revision": "73eb5c571b2c1b9b8ef34426804005e6",
+    "url": "images/f/f_clothing_f_l_arm_1.png"
+  },
+  {
+    "revision": "28dc6f9a3ec30630e6b842d5de961622",
+    "url": "images/f/f_clothing_f_l_leg_2.png"
+  },
+  {
+    "revision": "c17e8670c8aeeaaae3e97fa2c58ec59c",
+    "url": "images/f/f_clothing_f_r_leg_1.png"
+  },
+  {
+    "revision": "455cfc294660d1ddc16159b96d697e3f",
+    "url": "images/f/f_b_torso.png"
+  },
+  {
+    "revision": "f13d685eaf9777615f1c94d2cd99cd5e",
+    "url": "images/f/f_clothing_f_r_leg_2.png"
+  },
+  {
+    "revision": "2d89f0d3e9c1a7af3b78563ffe1820c6",
+    "url": "images/f/f_clothing_f_l_leg_1.png"
+  },
+  {
+    "revision": "7cb1ed0ccafcd35cb70ec6c0bab2b56b",
+    "url": "images/f/f_clothing2_f_l_arm_1.png"
+  },
+  {
+    "revision": "4790450262cb6d47935a96ef176677cb",
+    "url": "images/f/f_clothing2_f_r_arm_1.png"
+  },
+  {
+    "revision": "f008d4fb987e60e6d2d388c75342cb9a",
+    "url": "images/f/f_clothing_f_torso.png"
+  },
+  {
+    "revision": "2d89f0d3e9c1a7af3b78563ffe1820c6",
+    "url": "images/f/f_clothing2_f_l_leg_1.png"
+  },
+  {
+    "revision": "d957fd49b0ace2adf21139ee9b99b5eb",
+    "url": "images/f/f_clothing_shirt_bottom.png"
+  },
+  {
+    "revision": "28dc6f9a3ec30630e6b842d5de961622",
+    "url": "images/f/f_clothing2_f_l_leg_2.png"
+  },
+  {
+    "revision": "c17e8670c8aeeaaae3e97fa2c58ec59c",
+    "url": "images/f/f_clothing2_f_r_leg_1.png"
+  },
+  {
+    "revision": "be6ac83ac816c88f5cdbf5702ecda59f",
+    "url": "images/f/f_f_l_arm_1.png"
+  },
+  {
+    "revision": "f13d685eaf9777615f1c94d2cd99cd5e",
+    "url": "images/f/f_clothing2_f_r_leg_2.png"
+  },
+  {
+    "revision": "c73736258f43cb15752fb30d3ac3b71c",
+    "url": "images/f/f_clothing2_shirt_bottom.png"
+  },
+  {
+    "revision": "ce02050226b379b962ae0e757b271352",
+    "url": "images/f/f_f_l_leg_1.png"
+  },
+  {
+    "revision": "bbe5919c8daf4a6a8ee01a49d3f0dcc9",
+    "url": "images/f/f_f_l_leg_3.png"
+  },
+  {
+    "revision": "1c35f1716c6539a572e6f3868dbc4fd0",
+    "url": "images/f/f_f_l_leg_2.png"
+  },
+  {
+    "revision": "957e94600a1a8565634de376834c5026",
+    "url": "images/f/f_clothing2_f_torso.png"
+  },
+  {
+    "revision": "1e0340019a61c523727ebdef69169332",
+    "url": "images/f/f_f_l_arm_2.png"
+  },
+  {
+    "revision": "dfb05e70a4d8e0663f6310ee0803d662",
+    "url": "images/f/f_f_r_arm_3.png"
+  },
+  {
+    "revision": "d81bfc1cf3c21b95842e7345eafcdd78",
+    "url": "images/f/f_f_l_arm_3.png"
+  },
+  {
+    "revision": "0290c2849bd5711600f2aff2c683cf26",
+    "url": "images/f/f_f_r_arm_1.png"
+  },
+  {
+    "revision": "635c8700531718b8773abfc533df3859",
+    "url": "images/f/f_f_r_leg_3.png"
+  },
+  {
+    "revision": "5c60f33881f16edb5441750e4f199473",
+    "url": "images/f/f_f_torso.png"
+  },
+  {
+    "revision": "55bdbb9732010042ae803a4e2ea38002",
+    "url": "images/f/f_f_r_arm_2.png"
+  },
+  {
+    "revision": "2d388fb52e92b9b5c86d1a854f12db3d",
+    "url": "images/f/f_f_r_leg_2.png"
+  },
+  {
+    "revision": "d3fb67143a65fb93bccce51a5b7c8f18",
+    "url": "images/f/f_hair_s_hair_1.png"
+  },
+  {
+    "revision": "a141adb2ed27f0bfe23a0bcf9cc76add",
+    "url": "images/f/f_f_r_leg_1.png"
+  },
+  {
+    "revision": "0db38132dbb2cbde2e0e84a1cfd4dc6a",
+    "url": "images/f/f_hair_s_hair_2.png"
+  },
+  {
+    "revision": "104b6959c829b75e18bd75d6536348d4",
+    "url": "images/f/f_clothing_f_r_arm_1.png"
+  },
+  {
+    "revision": "fe1cf5115e940c590612c7aeb2708fe0",
+    "url": "images/f/f_hair_s_hair_front_1.png"
+  },
+  {
+    "revision": "0cf8549bd8a310a09aa69078fc4d41ac",
+    "url": "images/f/f_head_s_ear.png"
+  },
+  {
+    "revision": "0225a4642b86044da56d155c8dbbe5ec",
+    "url": "images/f/f_head_s_eye_fg.png"
+  },
+  {
+    "revision": "08ecfc60b72ce1b9a2d34077d3f96a77",
+    "url": "images/f/f_head_f_neck.png"
+  },
+  {
+    "revision": "4b216e328c5065696fed32dcac48bf3c",
+    "url": "images/f/f_head_s_eye_bg.png"
+  },
+  {
+    "revision": "c5999c846eb8798862d42d730b9960c3",
+    "url": "images/f/f_head_s_eyebrow.png"
+  },
+  {
+    "revision": "66cc70a283122e6e01b2e303245a37ad",
+    "url": "images/f/f_s_head.png"
+  },
+  {
+    "revision": "c24ae62d1ea0465f03323e9bc9603dc0",
+    "url": "images/f/f_longsword.png"
+  },
+  {
+    "revision": "1e661ad6c12e16b948f68e07ed1449de",
+    "url": "images/f/f_head_s_head.png"
+  },
+  {
+    "revision": "14963e177c9b62d66139f98bbd95ce87",
+    "url": "images/f/f_hair_s_hair_back_2.png"
+  },
+  {
+    "revision": "9919e0b09f1c57f7de0fd3718d624930",
+    "url": "images/f/f_s_r_leg_3.png"
+  },
+  {
+    "revision": "ed93b1dc082d4ca4cacf63e80628e289",
+    "url": "images/f/f_sabre.png"
+  },
+  {
+    "revision": "4b1b8474ef15c33e6e61af6e48d00880",
+    "url": "images/f/f_s_torso.png"
+  },
+  {
+    "revision": "06daed94def451b9639192266581b6d2",
+    "url": "images/f/f_s_r_arm_1.png"
+  },
+  {
+    "revision": "049fe7f8ca25589747e92054df451388",
+    "url": "images/m/m_clothing_f_l_leg_1.png"
+  },
+  {
+    "revision": "d2b626e1e492f9385802bd694da33615",
+    "url": "images/m/m_clothing_f_l_arm_1.png"
+  },
+  {
+    "revision": "13ac31d49eff0faa6a0e3c992e99d7c9",
+    "url": "images/m/m_clothing_f_l_leg_2.png"
+  },
+  {
+    "revision": "c073ba43053b9046542d7f18eb26c836",
+    "url": "images/m/m_clothing_f_r_leg_1.png"
+  },
+  {
+    "revision": "70c062b8a54e214d674a1db4f76f9c1a",
+    "url": "images/m/m_clothing_f_torso.png"
+  },
+  {
+    "revision": "9e4b4ff3f35f1b24f2b90307e13a7934",
+    "url": "images/m/m_clothing_f_r_arm_1.png"
+  },
+  {
+    "revision": "9d86fb7e2210dd1d3fd70749bcd0151e",
+    "url": "images/m/m_clothing_shirt_bottom.png"
+  },
+  {
+    "revision": "fffc4cc8ac5837b81041a4c8199dd988",
+    "url": "images/m/m_clothing_f_r_leg_2.png"
+  },
+  {
+    "revision": "81988d662ab80cc083d219433ef1439c",
+    "url": "images/m/m_clothing2_f_r_arm_1.png"
+  },
+  {
+    "revision": "6f93d9e0767ea181aada4de4f614f8e4",
+    "url": "images/m/m_clothing2_f_l_arm_1.png"
+  },
+  {
+    "revision": "fffc4cc8ac5837b81041a4c8199dd988",
+    "url": "images/m/m_clothing2_f_r_leg_2.png"
+  },
+  {
+    "revision": "13ac31d49eff0faa6a0e3c992e99d7c9",
+    "url": "images/m/m_clothing2_f_l_leg_2.png"
+  },
+  {
+    "revision": "049fe7f8ca25589747e92054df451388",
+    "url": "images/m/m_clothing2_f_l_leg_1.png"
+  },
+  {
+    "revision": "c073ba43053b9046542d7f18eb26c836",
+    "url": "images/m/m_clothing2_f_r_leg_1.png"
+  },
+  {
+    "revision": "7b33959c76c38acce7ae0ad4927e01eb",
+    "url": "images/m/m_clothing2_shirt_bottom.png"
+  },
+  {
+    "revision": "95863682bf7d11ab15303ada69765912",
+    "url": "images/m/m_f_l_leg_1.png"
+  },
+  {
+    "revision": "695e4d4998547a82c22a6f68f7279fe9",
+    "url": "images/m/m_f_l_arm_2.png"
+  },
+  {
+    "revision": "0783ef3d200f4af5c3ec5e0b4c9c8f73",
+    "url": "images/m/m_clothing2_f_torso.png"
+  },
+  {
+    "revision": "38bffd20449e88227744bd5fd4b796bb",
+    "url": "images/m/m_f_l_arm_1.png"
+  },
+  {
+    "revision": "aa24f866bf18fffc92499060f35bb07b",
+    "url": "images/m/m_f_l_leg_2.png"
+  },
+  {
+    "revision": "b0a416602a032c7305b37ce29360f42e",
+    "url": "images/m/m_f_l_arm_3.png"
+  },
+  {
+    "revision": "20e0a0b7085a072b70b1eb03e0323584",
+    "url": "images/m/m_f_r_arm_1.png"
+  },
+  {
+    "revision": "ac3bd3ec911a2a61e0f30f1df4043658",
+    "url": "images/m/m_f_neck.png"
+  },
+  {
+    "revision": "8df7b47c0ac422adfe024f8859b1e961",
+    "url": "index.html"
+  },
+  {
+    "revision": "76ac39bb047326ed191e9bb99e1667d6",
+    "url": "images/m/m_f_r_leg_1.png"
+  },
+  {
+    "revision": "1509ea2716632db674f3695b5f502996",
+    "url": "images/m/m_f_torso.png"
+  },
+  {
+    "revision": "7d40d9ac5e628c8f4ab5e683b11e09ba",
+    "url": "zip-js/z-worker.js"
+  },
+  {
+    "revision": "b9e60c4d49694383b85afb51f4e70839",
+    "url": "images/m/m_f_r_arm_2.png"
+  },
+  {
+    "revision": "a835fd7e98246b4deab56b44badfcca5",
+    "url": "images/m/m_f_r_leg_3.png"
+  },
+  {
+    "revision": "4c0d2dc3ce7ab6a01837f2e589067fc1",
+    "url": "images/m/m_s_eye_bg.png"
+  },
+  {
+    "revision": "a70615f5a317f8e140fcba91988b35c7",
+    "url": "images/m/m_f_r_leg_2.png"
+  },
+  {
+    "revision": "17613622acdbe37209553d23f00a697f",
+    "url": "images/m/m_s_eyebrow.png"
+  },
+  {
+    "revision": "699b1b908e9ad473cc29ff58388535a9",
+    "url": "images/m/m_s_eye_fg.png"
+  },
+  {
+    "revision": "3b9af237ea6d1210b195336611d2a2f4",
+    "url": "images/m/m_s_hair_back_2.png"
+  },
+  {
+    "revision": "8bf7e1dea533a7cb4ff6c3082e374163",
+    "url": "images/m/m_s_hair_2.png"
+  },
+  {
+    "revision": "fa046395062ba429a3cce35223a3cb0a",
+    "url": "images/m/m_s_hair_front_2.png"
+  },
+  {
+    "revision": "0b29237ce2a7afda47684adb519d344d",
+    "url": "images/m/m_longsword.png"
+  },
+  {
+    "revision": "0050e946f2be4ae827a8e79a664c859e",
+    "url": "images/m/m_s_hair_front_1.png"
+  },
+  {
+    "revision": "fd26635b6489858e385d5915395976f4",
+    "url": "images/m/m_s_hair_1.png"
+  },
+  {
+    "revision": "a27e34ef9d4b4e5b80acc443c504aaec",
+    "url": "images/m/m_s_ear.png"
+  },
+  {
+    "revision": "dcb31d982e8b9aac7b1eb633d5232364",
+    "url": "images/m/m_s_head.png"
+  },
+  {
+    "revision": "fd26635b6489858e385d5915395976f4",
+    "url": "images/m/m_s_hair.png"
+  },
+  {
+    "revision": "9365f982d209fc3691334f0a339def41",
+    "url": "zip-js/.jshintrc"
+  },
+  {
+    "revision": "97ef69c093c85afd3ee9f8295206b047",
+    "url": "images/m/m_sabre.png"
+  },
+  {
+    "revision": "0b905934d7271bd0d03624ad556df7a0",
+    "url": "zip-js/mime-types.js"
+  },
+  {
+    "revision": "d4e3a2a82db29526b20ce9314ce835fe",
+    "url": "zip-js/deflate.js"
+  },
+  {
+    "revision": "0050e946f2be4ae827a8e79a664c859e",
+    "url": "images/m/m_s_hair_front.png"
+  },
+  {
+    "revision": "4abd21a23d6a5519b9b692f98d67e7c5",
+    "url": "zip-js/pako/codecs.js"
+  },
+  {
+    "revision": "eb70f64ee76b4132ac3c5df73fb99d76",
+    "url": "zip-js/tests/config.js"
+  },
+  {
+    "revision": "562d23f345ba9d9325c5f8112c9248a3",
+    "url": "zip-js/tests/base64.js"
+  },
+  {
+    "revision": "fd952b261e960d0d7c8498569bee75d6",
+    "url": "zip-js/inflate.js"
+  },
+  {
+    "revision": "760913ebacfe547d9a1ee99374d1e43f",
+    "url": "zip-js/tests/test1.html"
+  },
+  {
+    "revision": "1844af1441424e7245f6231f47303b5f",
+    "url": "zip-js/tests/arraybuffer.js"
+  },
+  {
+    "revision": "95cede97388d3e764db282653ef62d37",
+    "url": "zip-js/tests/test10.js"
+  },
+  {
+    "revision": "9c3fd0ce1c274a4c38175090d072394f",
+    "url": "zip-js/tests/test10.html"
+  },
+  {
+    "revision": "695fa4fe8fc4224b284fcf8511a139e3",
+    "url": "zip-js/tests/test11.html"
+  },
+  {
+    "revision": "3d5c6640d26862f466876ee9450273a3",
+    "url": "zip-js/tests/test1.js"
+  },
+  {
+    "revision": "47f97c1c74e9213c6352bb14548383b6",
+    "url": "zip-js/tests/test11.js"
+  },
+  {
+    "revision": "14a4da082ac65356cbecf8df4414b8e4",
+    "url": "zip-js/tests/test12.html"
+  },
+  {
+    "revision": "e4382a322ea93c2bab6f1f118e648d74",
+    "url": "zip-js/tests/test12.js"
+  },
+  {
+    "revision": "bd12ea8442c67a4f97b9ce0b28e8e437",
+    "url": "zip-js/tests/test13.html"
+  },
+  {
+    "revision": "85b40beac94c4768a98ecfc53b028b27",
+    "url": "zip-js/tests/lorem.zip"
+  },
+  {
+    "revision": "296f8b00d7cf6884c48db5afb60eaf96",
+    "url": "zip-js/tests/test14.html"
+  },
+  {
+    "revision": "c70d91419a886e014b25138f70b13544",
+    "url": "zip-js/tests/test13.js"
+  },
+  {
+    "revision": "448d0800c84b59380b9f1f3aac9e8c66",
+    "url": "zip-js/tests/test15.html"
+  },
+  {
+    "revision": "f6e3fea543f965f60dd3c6a4a522976e",
+    "url": "zip-js/tests/dataview.js"
+  },
+  {
+    "revision": "2e73d6cd3680a08441c902efc890b157",
+    "url": "zip-js/tests/test16.html"
+  },
+  {
+    "revision": "9c7db9f5bcb57438bccbf048efda5eff",
+    "url": "zip-js/tests/test15.js"
+  },
+  {
+    "revision": "dd09509d89e1d7aa889c43d1676081b8",
+    "url": "zip-js/tests/test17.html"
+  },
+  {
+    "revision": "495347bbce33c4733bee7ef6b10b55b3",
+    "url": "zip-js/tests/lorem.txt"
+  },
+  {
+    "revision": "f14846199ecd449d62b5f16d280753ac",
+    "url": "zip-js/tests/test18.html"
+  },
+  {
+    "revision": "35a40c3d64f37d9bd465ad5a17f740c9",
+    "url": "zip-js/tests/test17.js"
+  },
+  {
+    "revision": "46f6fac55a54ed8d327e985238aa13be",
+    "url": "zip-js/tests/test14.js"
+  },
+  {
+    "revision": "797a0a6fe59a29e1e4d5ac2efdb1d926",
+    "url": "zip-js/tests/test2.html"
+  },
+  {
+    "revision": "82167edb99a23083b26d41e06b441a69",
+    "url": "zip-js/tests/test2.js"
+  },
+  {
+    "revision": "b42fd4058650004d06b8ac6891bdca81",
+    "url": "zip-js/tests/test3.html"
+  },
+  {
+    "revision": "7ded7a531448e1f662ce6cc35474bc62",
+    "url": "zip-js/tests/test3.js"
+  },
+  {
+    "revision": "f693d21ad54c88accd21adec719eb955",
+    "url": "zip-js/tests/test4.html"
+  },
+  {
+    "revision": "50494731bd84ea56be1e43f1bc8b8595",
+    "url": "zip-js/tests/test19-codecs-interop.html"
+  },
+  {
+    "revision": "f8e3e28cf6cf19970abcc2955c68c0df",
+    "url": "zip-js/tests/test5.html"
+  },
+  {
+    "revision": "6f737d33c3d1021d7dd2d6743e570715",
+    "url": "zip-js/tests/test16.js"
+  },
+  {
+    "revision": "bae631cb51d20df528fa0c508aac4575",
+    "url": "zip-js/tests/test6.html"
+  },
+  {
+    "revision": "1e509807b93d850d0d5e07d9bff8d993",
+    "url": "zip-js/tests/test5.js"
+  },
+  {
+    "revision": "cddfe15e10a0c0f9c1e75b1feda65ba4",
+    "url": "zip-js/tests/test7.html"
+  },
+  {
+    "revision": "1aa6b66ea462b04ae4fd148cd083cde9",
+    "url": "zip-js/tests/test7.js"
+  },
+  {
+    "revision": "8dda4a5df1e7eeffd56eb129cd53f00c",
+    "url": "zip-js/tests/test8.html"
+  },
+  {
+    "revision": "39986a804afc492471b4df56269eb588",
+    "url": "zip-js/tests/test8.js"
+  },
+  {
+    "revision": "115fe9c337cf81ca80c57e82e3dbe8de",
+    "url": "zip-js/tests/test9.html"
+  },
+  {
+    "revision": "6f0ae386893d65177912f46bcd539d97",
+    "url": "zip-js/tests/test9.js"
+  },
+  {
+    "revision": "11f29c33e648fe58b3721bc523df05d5",
+    "url": "zip-js/tests/util.js"
+  },
+  {
+    "revision": "802870708b3875544e072a9c195dbcec",
+    "url": "zip-js/tests/test6.js"
+  },
+  {
+    "revision": "c37900774979be9b9cd648e435936c8a",
+    "url": "zip-js/tests/lorem2.zip"
+  },
+  {
+    "revision": "045647387799cc888d1493afd4c3588a",
+    "url": "zip-js/tests/test4.js"
+  },
+  {
+    "revision": "10b73276ffb7b0cd65a21104183012b6",
+    "url": "zip-js/tests/test18.js"
+  },
+  {
+    "revision": "7eb585103a784d2aa99d90d14c1a6147",
+    "url": "nox_pwa.webmanifest"
+  },
+  {
+    "revision": "6b261544f39d23aa6a9fc8d70e6fada1",
+    "url": "zip-js/tests/lorem_store.zip"
+  },
+  {
+    "revision": "75e20b37354b0177411681eb2a764277",
+    "url": "images/f/f_b_l_arm_3.png"
+  },
+  {
+    "revision": "865eeb56863464cb56bd5f28b619b396",
+    "url": "icons/icon-256.png"
+  },
+  {
+    "revision": "53c4c6878ed0b36bfb058a0bd66903a7",
+    "url": "icons/icon-128.png"
+  },
+  {
+    "revision": "01f3504108bc721f9b37",
+    "url": "bundle.js"
+  }
+];
--- character_preview/service-worker.js
+++ character_preview/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.14793eb1ef7bd2d0b371f656dd4d5a23.js"
+  "precache-manifest.b019ddb2c0901b9fec88933a7bde3b1a.js"
 );
 
 workbox.skipWaiting();
