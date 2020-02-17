228055c4fa1737d97e579335432a4a0e6ffa0ddd
Maximilian Jensen <maxjen@gmail.com>
2020-02-18 00:55:42 +0100

    Added parallax layers

--- editor/service-worker.js
+++ editor/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.76f20b763c273f00e2d517b51058a95c.js"
+  "precache-manifest.f2be168f9d2fadc8ae4a76f2a9ea9f24.js"
 );
 
 workbox.skipWaiting();
